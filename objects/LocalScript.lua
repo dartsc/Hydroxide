6db692a5a7ff49b638ec7123d03a2b2f562c8ca6
Dzgak <dev.dzgak@gmail.com>
2024-07-30 10:53:12 +0600

    Update LocalScript.lua

--- objects/LocalScript.lua
+++ objects/LocalScript.lua
@@ -2,12 +2,12 @@ local LocalScript = {}
 
 function LocalScript.new(instance)
     local localScript = {}
-    local closure = getScriptClosure(instance)
+    local closure = getscriptclosure(instance)
 
     localScript.Instance = instance
-    localScript.Environment = getSenv(instance)
-    localScript.Constants = getConstants(closure)
-    localScript.Protos = getProtos(closure)
+    localScript.Environment = getsenv(instance)
+    localScript.Constants = getconstants(closure)
+    localScript.Protos = getprotos(closure)
 
     return localScript
 end
